d8dc1c72e8320d5be6e25dacc197eba9b53c694c
Sebastian Uddén <sebastian.udden@eurostep.com>
2017-10-31 22:39:40 +0100

    Fixed the course value to a more reasonable sum, tests should clear

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './index.css';
 import numeral from 'numeral';
 var exports = module.exports = {};
 
-const courseValue = numeral(2000).format('$0,0.00');
+const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${courseValue} for this awesome course!`);
 exports.courseValue = courseValue;
